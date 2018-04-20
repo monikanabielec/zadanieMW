123f331828767fb59f387b686e616e47dd8eaf16
Unknown <monika.nabielec@gmail.com>
2018-04-20 09:51:31 +0200

    Initial

---
+++ Program/Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
